ed60a7879a17f44c12ee956a6699a65433a038af
WoolimLee <woolimlee.dev@gmail.com>
2024-06-17 10:39:43 +0900

    전투 씬에서 일시정지 기능 추가, 후퇴 시 필드씬으로 이동

--- Assets/Scripts/UI/Pause/BattlePause.cs
+++ Assets/Scripts/UI/Pause/BattlePause.cs
@@ -1,17 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.SearchService;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class FieldPause : Pause
+public class BattlePause : Pause
 {
     [SerializeField]
     private RectTransform pauseOutsideBorder;
+    [SerializeField]
+    private RectTransform continueButton;
+    [SerializeField]
+    private RectTransform exitButton;
 
     public override void Start()
     {
         base.Start();
 
         AddToggleEvent(pauseOutsideBorder);
+        AddToggleEvent(continueButton);
+        AddEvent(exitButton, ExitToFieldScene);
         TogglePauseCanvas(pauseOutsideBorder);
     }
 
@@ -20,5 +28,10 @@ public class FieldPause : Pause
         base.TogglePauseState();
         TogglePauseCanvas(pauseOutsideBorder);
         Time.timeScale = isPause ? 0 : 1;
+    }
+
+    private void ExitToFieldScene()
+    {
+        SceneManager.LoadScene(0);
     }
 }
--- Assets/Scripts/UI/Pause/Pause.cs
+++ Assets/Scripts/UI/Pause/Pause.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UIElements;
@@ -53,5 +54,28 @@ public class Pause : MonoBehaviour
 
         entry.callback.AddListener((eventData) => TogglePauseState());
         targetTrigger.triggers.Add(entry);
+    }
+
+    protected void AddEvent(RectTransform target, Action callback)
+    {
+        EventTrigger targetTrigger = target.GetComponent<EventTrigger>();
+
+        if (targetTrigger == null)
+        {
+            Debug.LogError("해당 컴포넌트에는 이벤트 트리거가 등록되지 않았어요.");
+            return;
+        }
+
+        EventTrigger.Entry entry = new EventTrigger.Entry();
+        entry.eventID = EventTriggerType.PointerClick;
+
+        entry.callback.AddListener((eventData) => callback());
+        targetTrigger.triggers.Add(entry);
+    }
+
+    private void OnDestroy()
+    {
+        isPause = false;
+        Time.timeScale = 1;
     }
 }
