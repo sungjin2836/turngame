dc1d31f42ab99e1f181adb1443a94686d91e53b9
sungjin <sungjin2836@naver.com>
2024-06-18 09:28:45 +0900

    Gizmos로 처리되던 Ray update에서 실행되게 변경

--- Assets/TargetRayCast.cs
+++ Assets/TargetRayCast.cs
@@ -17,17 +17,15 @@ public class TargetRayCast : MonoBehaviour
         _controller = GetComponent<FieldUIController>();
     }
 
-    private void OnDrawGizmos()
+    private void Update()
     {
-        Gizmos.color = _color;
-
         if (Physics.BoxCast(transform.position, transform.lossyScale / 2f, transform.forward, out RaycastHit hit, transform.rotation, _maxDistance))
         {
-            Gizmos.DrawRay(transform.position, transform.forward * hit.distance);
+            Debug.DrawRay(transform.position, transform.forward * hit.distance, _color);
+            Debug.DrawLine(transform.position + transform.forward * hit.distance - transform.lossyScale / 2f,
+                           transform.position + transform.forward * hit.distance + transform.lossyScale / 2f, _color);
 
-            Gizmos.DrawWireCube(transform.position + transform.forward * hit.distance, transform.lossyScale);
-            
-            if(hit.collider.gameObject.CompareTag("Enemy"))
+            if (hit.collider.gameObject.CompareTag("Enemy"))
             {
                 if (_controller == null)
                 {
@@ -48,11 +46,49 @@ public class TargetRayCast : MonoBehaviour
                 _controller = GetComponent<FieldUIController>();
                 _controller.NoDetectEnemy();
             }
-            
         }
         else
         {
-            Gizmos.DrawRay(transform.position, transform.forward * _maxDistance);
+            Debug.DrawRay(transform.position, transform.forward * _maxDistance, _color);
         }
     }
+
+    //private void OnDrawGizmos()
+    //{
+    //    Gizmos.color = _color;
+
+    //    if (Physics.BoxCast(transform.position, transform.lossyScale / 2f, transform.forward, out RaycastHit hit, transform.rotation, _maxDistance))
+    //    {
+    //        Gizmos.DrawRay(transform.position, transform.forward * hit.distance);
+
+    //        Gizmos.DrawWireCube(transform.position + transform.forward * hit.distance, transform.lossyScale);
+            
+    //        if(hit.collider.gameObject.CompareTag("Enemy"))
+    //        {
+    //            if (_controller == null)
+    //            {
+    //                _controller = GetComponent<FieldUIController>();
+    //                Debug.Log($"if (_controller == null)이면 실행 {_controller}");
+    //            }
+
+    //            if (_controller != null)
+    //            {
+    //                _gameObject = hit.collider.gameObject;
+    //                rayEnemy = _gameObject.GetComponent<Enemy>();
+    //                Debug.Log($"적 정보 나오는지 {rayEnemy.charName}");
+    //                _controller.DetectEnemy(rayEnemy);
+    //            }
+    //        }
+    //        else
+    //        {
+    //            _controller = GetComponent<FieldUIController>();
+    //            _controller.NoDetectEnemy();
+    //        }
+            
+    //    }
+    //    else
+    //    {
+    //        Gizmos.DrawRay(transform.position, transform.forward * _maxDistance);
+    //    }
+    //}
 }
